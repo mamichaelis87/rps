d00c3fa5a5b10c8fcda06841ec951ea019d7e2ec
mamichaelis87 <mallenm1993@gmail.com>
2022-12-12 18:17:35 -0500

    Write function for computerChoice

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
         
 console.log("It's going down.");
 
+// Get a choice from the computer
+//  - initialize computerChoice variable
+//  - randomly assign rock, paper, or scissors to computerChoice
+function getComputerChoice() {
+    let computerChoice = Math.random()
+        console.log(computerChoice);
+    if (computerChoice <= 1/3) {
+        console.log("rock");
+        return "rock";
+    }
+    else if (computerChoice <= 2/3) {
+        console.log("paper");
+        return "paper";
+    }
+    else {
+        console.log("scissors");
+        return "scissors";
+    }
+}
+
+getComputerChoice();
